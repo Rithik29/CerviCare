4cfb850287421337bb3c126f32cd129b702badec
Anjita Gargi <anjitagargi@gmail.com>
2023-11-05 00:43:13 +0530

    [CerviCare] : Add frontend files

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,31 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Dashboard, Auth } from './layouts'
+import OrderDetails from './pages/dashboard/orderDetails'
+import ResetPassword from './pages/dashboard/ResetPassword'
+import ForgotPassword from './pages/dashboard/ForgotPassword'
+import { useEffect } from 'react'
+import { SignIn, SignUp } from './pages/auth'
+
+function App() {
+  // useEffect(() => {
+  //   localStorage.setItem('token', 'yuaihdbaonaosnonoa')
+  // }, [])
+  const token = localStorage.getItem('token')
+
+  return (
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/dashboard/orderDetail/:id" element={<OrderDetails />} />
+      <Route path="/resetpassword" element={<ResetPassword />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+      <Route path="/resetpassword" element={<ResetPassword />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/auth/*" element={<Auth />} />
+      {/* <Route path="*" element={<Navigate to="/auth/sign-in" replace />} /> */}
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  )
+}
+
+export default App
